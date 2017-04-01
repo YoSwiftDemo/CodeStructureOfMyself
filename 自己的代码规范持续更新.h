4cc2da0b2c3a3337396ab9aa01046063f8d576e4
岳克奎 <738816656@qq.com>
2017-04-01 09:45:27 +0800

    1

--- 自己的代码规范持续更新.h
+++ 自己的代码规范持续更新.h
@@ -18,17 +18,18 @@
    写在自己屁股后面，加载的时候，记着让模块管理中心控制加载，绕一圈，后期写完你就知道好处大大的好
 3 代码外部：
     总头文件：
-    控制中心层：
-    API管理中心：
-    数据管理中心：
+    控制中心层：   目前是写成single
+    API管理中心：  代码千万别嫩成胖model。代每个模块应该有自己的API层，比如悬浮直播和音乐 毛线关系木有，API要分开，要分家！
+    数据管理中心：  音乐模块包含了LRC 数据 数据源的处理 包括对mdoel的升级扩展
     UI：
-    扩展：
+    扩展：       主要是xib动态桥接 处理View    发现xib动态桥接简直是神器！
     测试资源：
-4.代码内部：
+    Model
+    LRC 相关 ： 为什么把他单独拿出来，因为我们是双行（上下行）歌词  类似KTV 模式，关于LRC自己相关的东西 放在自个m文件的+方法里，不要乱放。通过调度中心去交互，也不要乱交互，绕道调度中心。
+    等等
+4.//代码内部：
     #prama mark -申明属性
-    #prama mark -subViewS  layout
-    #prama mark -viewWillAppear
-    #prama mark - viewDidAppear
+
     做Notification的监听之类的事情  listenerAndOther   增加监听等
     #prama mark -life cycle
     控制生命周期区域
@@ -36,9 +37,9 @@
     只做addSubview的事情 。xib下这块基本上木有一行代码
     #pragma mark-system  delegate
     实现系统代理区域
-    #pragma mark- custom  delegate
+    #pragma mark- myself  delegate
     自定义代理区域
-    #pragma mark -evebt response
+    #pragma mark -event response
     事件响应区域
     #pragma mark -public  methods
     公有方法区域
@@ -46,17 +47,27 @@
     私人方法区域
     #pragma mark -getters and setters
     属性的初始化，则交给getter去做
-5.代码注释
+
+  /*
+   自己目前用的排列顺序这样：
+   life cycle-> viewDidLoad-> public methods->+方法-->private methods->-event response-> -getters and setters
+   +方法应该归属于public
+   */
+
+5.//代码注释
+    h文件简单注释
+    m 文件越详细越好
+
     h里简单注释  ：
     @brief         方法作用简单说明
     @discusson 一般是情况分类或需要注意问题
     @ use          指明 大致在什么逻辑或具体哪个类哪里用到这个方法
     m文件基本注释很详细
     @brief
-    @@prama      参数最好注释下。有时候比如superViewControlelr最好指明是哪个类，特别是互动直播 逻辑层和UI层我就混了一次。改完，心都碎了
-    @discusson
-    @discusson
+    @prama      参数最好注释下。有时候比如superViewControlelr最好指明是哪个类，特别是互动直播 逻辑层和UI层我就混了一次。改完，心都碎了
+    @Function
     @ use
     @ return
+6.//代码完成差不多 才去写下思维导图。
 
 #endif /* Header_h */
